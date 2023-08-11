5aa1d4f23df90053733ec9b8a50b92a4c3ded23b
jhonbg09 <jhon.betancourthg09@gmail.com>
2023-08-10 22:04:23 -0500

    creando modelos en sequelize

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
-const server = require ("./src/app");
+const server = require("./src/app");
+const {conn} = require("./src/db");
 
 const port = 3001
+
 server.listen(port, ()=>{
+    conn.sync();
     console.log(`listening on port ${port}`);
 })
--- api/src/db.js
+++ api/src/db.js
@@ -1,3 +1,15 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname');
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+
+//const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname')
+const sequelize = new Sequelize(
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`
+);
+
+
+
+module.exports = {
+  conn: sequelize //se debe importar en el index
+}
