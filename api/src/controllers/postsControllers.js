63e23159b421271748b8587f6b484688b4643026
jhonbg09 <jhon.betancourthg09@gmail.com>
2023-08-14 16:22:20 -0500

    creacion FK

--- api/src/controllers/postsControllers.js
+++ api/src/controllers/postsControllers.js
@@ -1,9 +1,11 @@
-const {posts} = require('../db')
+const { posts } = require("../db");
 
-const createPostsHandlerDB = async (title, body) =>{
-    return await posts.create({title, body});
-}
+const createPostsDB = async (title, body, userId) => {
+  const newPost = await posts.create({ title, body });
+  await newPost.setUser(userId); //set user es un metodo de sequilize que nos genera la clave foranea;
+  return newPost;//para aclarar en clase 3 de repaso jorge vega min 1:13:00
+};
 
 module.exports = {
-    createPostsHandlerDB
-}
+  createPostsDB,
+};
--- api/src/handler/postsHandler.js
+++ api/src/handler/postsHandler.js
@@ -1,15 +1,15 @@
 
-const {  createPostsHandlerDB } = require('../controllers/postsControllers')
+const {  createPostsDB } = require('../controllers/postsControllers')
 
 const postsHandler = (req, res) => {
   res.status(200).send("Estoy consultando los posts");
 };
 
 const createPostsHandler = async (req, res) => {
-  const {title, body} = req.body;
+  const {title, body, userId} = req.body;
   try {
-    const response = await  createPostsHandlerDB(title, body);
-    res.status(200).json(response);
+    const newPost = await  createPostsDB(title, body, userId);
+    res.status(201).json(newPost);//el status 201 nos indica que ha sido creado correctamente el post para este ejemplo
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
